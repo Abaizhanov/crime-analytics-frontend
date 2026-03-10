b8452ed54782a54684605f4c2cbeffbcbde136fa
Abaizhanov <122878092+Abaizhanov@users.noreply.github.com>
2026-03-10 23:37:14 +0500

    version 1.0

--- src/app/components/CrimeMap.js
+++ src/app/components/CrimeMap.js
@@ -1,43 +1,100 @@
 'use client'
+
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import {MapContainer, TileLayer, Marker, Popup, useMapEvents} from 'react-leaflet'
 import MarkerClusterGroup from 'react-leaflet-cluster'
-import { useEffect, useState } from 'react'
+import {useState} from 'react'
+import L from "leaflet"
+
+const createClusterIcon = (cluster) => {
+
+  const markers = cluster.getAllChildMarkers()
+
+  let total = 0
+
+  markers.forEach(marker => {
+    total += marker.options.count || 1
+  })
+
+  return L.divIcon({
+    html: `<div class="crime-cluster">${total}</div>`,
+    className: "crime-cluster-wrapper",
+    iconSize: L.point(40, 40)
+  })
+}
+
+function MapEvents({setCrimes}) {
+
+  const map = useMapEvents({
+
+    load: fetchData,
+    moveend: fetchData
+
+  })
+
+  function fetchData() {
+
+    const bounds = map.getBounds()
+    const zoom = map.getZoom()
+
+    const north = bounds.getNorth()
+    const south = bounds.getSouth()
+    const east = bounds.getEast()
+    const west = bounds.getWest()
+
+    fetch(`http://127.0.0.1:8000/api/map?north=${north}&south=${south}&east=${east}&west=${west}&zoom=${zoom}`)
+      .then(res => res.json())
+      .then(data => setCrimes(data))
+  }
+
+  return null
+}
 
 export default function CrimeMap() {
 
   const [crimes, setCrimes] = useState([])
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:8000/api/map?north=43.30&south=43.20&east=76.95&west=76.85')
-      .then(res => res.json())
-      .then(data => setCrimes(data))
-  }, [])
-
+  console.log("CRIMES:", crimes.length)
   return (
     <MapContainer
       center={[43.238949, 76.889709]}
       zoom={12}
-      style={{ height: '100vh', width: '100%' }}
+      style={{height: '100vh', width: '100%'}}
     >
+
       <TileLayer
         attribution='© OpenStreetMap'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      <MarkerClusterGroup>
+      <MapEvents setCrimes={setCrimes}/>
 
-        {crimes.map((crime, index) => (
-          <Marker key={index} position={[crime.latitude, crime.longitude]}>
+
+      <MarkerClusterGroup
+        iconCreateFunction={createClusterIcon}
+        disableClusteringAtZoom={17}
+      >
+
+        {crimes.map((cluster, index) => (
+          <Marker
+            key={`${cluster.lat}-${cluster.lng}-${cluster.count}`}
+            position={[
+              Number(cluster.lat),
+              Number(cluster.lng)
+            ]}
+            options={{count: cluster.count}}
+          >
             <Popup>
-              {crime.crime_name} ({crime.year})
+              {cluster.count
+                ? `${cluster.count} преступлений`
+                : `${cluster.crime_name} (${cluster.year})`}
             </Popup>
           </Marker>
         ))}
 
       </MarkerClusterGroup>
+
 
     </MapContainer>
   )
